c624e19792bd0bef0cf9afd72671cb5a2eba0b9b
Andre Bertuzzi <andreqbertuzzi@gmail.com>
2021-10-10 20:26:00 -0300

    Configuring account and network

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,17 @@ task("accounts", "Prints the list of accounts", async () => {
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
+// module.exports = {
+//   solidity: "0.6.6",
+//   networks: {
+//     ropsten: {
+//       url: `https://eth-ropsten.alchemyapi.io/v2/RLHBILMi2CAMNo21uHvUYXk7S84TdYEi`,
+//       accounts: [
+//         `0xfffffffffffffffffffffffffffffffffff`,
+//       ],
+//     },
+//   },
+// };
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
